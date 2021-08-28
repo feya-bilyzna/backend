edad86c1002b08112303612ddd81d192981b4cdf
Andrew <life.around.freaks@gmail.com>
2021-08-28 18:44:58 +0300

    Заменил User на Customer, добавил возможность делать заказы.

--- underwearshop/schema.py
+++ underwearshop/schema.py
@@ -2,12 +2,16 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphene.types.generic import GenericScalar
+from graphql import GraphQLError
 
 from underwearshop.models import (
     Product,
     ProductImage,
     ProductRemains,
     Category,
+    Customer,
+    Order,
+    OrderProduct,
 )
 
 from django.db.models import When, Case, Value, Sum
@@ -102,6 +106,81 @@ def slice_products(qs, page):
     ).order_by('-in_stock', '-id')[(page-1)*12:page*12]
 
 
+class OrderItem(graphene.InputObjectType):
+
+    remains_id = graphene.Int(required=True)
+    amount = graphene.Int(required=True)
+
+
+class MakeOrder(graphene.Mutation):
+
+    class Arguments:
+
+        contact_info = graphene.String(required=True)
+        orders_list = graphene.List(OrderItem, required=True)
+
+    result = graphene.String()
+
+    def mutate(root, info, contact_info, orders_list):
+
+        if len(orders_list) > 20:
+            return MakeOrder(result='ORDER TOO LARGE')
+
+        orders = set()
+        for order in orders_list:
+            if order.remains_id in orders:
+                raise GraphQLError('Remains id must be unique.')
+            orders.add(order.remains_id)
+
+        try:
+            customer = Customer.objects.get(contact_info=contact_info)
+        except Customer.DoesNotExist:
+            customer = Customer(contact_info=contact_info)
+            customer.save()
+
+        order = Order.objects.filter(
+            customer=customer, processed=False
+        ).order_by('id').first()
+        created = False
+        if order is None:
+            order = Order(customer=customer, processed=False)
+            created = True
+        
+        valid_remains = dict(ProductRemains.objects.filter(
+            id__in=orders
+        ).values_list('id', 'remains'))
+        positions = [
+            OrderProduct(
+                order=order,
+                productremains_id=order_item['remains_id'],
+                amount=order_item['amount']
+            ) for order_item in orders_list
+            if valid_remains.get(
+                order_item['remains_id'], 0
+            ) > order_item['amount']
+        ]
+
+        if len(positions) != len(orders_list):
+            return MakeOrder(result='REMAINS INCONSISTENCY ERROR')
+
+        if not positions:
+            order.delete()
+            return MakeOrder(result='DELETED')
+
+        if created:
+            order.save()
+
+        OrderProduct.objects.filter(order=order).delete()
+        OrderProduct.objects.bulk_create(positions)
+
+        return MakeOrder(result='CREATED' if created else 'UPDATED')
+
+
+class Mutation(graphene.ObjectType):
+
+    make_order = MakeOrder.Field()
+
+
 class Query(graphene.ObjectType):
 
     all_products = graphene.List(
@@ -116,6 +195,14 @@ class Query(graphene.ObjectType):
     product_by_id = graphene.Field(
         ProductType,
         id=graphene.Int(required=True),
+    )
+    products_by_ids = graphene.List(
+        ProductType,
+        ids=graphene.List(graphene.Int, required=True),
+    )
+    products_by_customer = graphene.List(
+        ProductType,
+        contact_info=graphene.String(),
     )
 
     def resolve_all_products(root, info, page=1):
@@ -146,7 +233,13 @@ class Query(graphene.ObjectType):
         except Product.DoesNotExist:
             return None
 
+    def resolve_products_by_ids(root, info, ids):
+
+        return Product.objects.prefetch_related(
+            *PRODUCT_PREFETCHES
+        ).filter(id__in=ids)
+
 
 schema = graphene.Schema(
-    query=Query,
+    query=Query, mutation=Mutation
 )
